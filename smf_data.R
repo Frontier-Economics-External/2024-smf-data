ec55345d0f772e69b0ad74d6592cb54750f0070f
Alex Latti <alex.latti@frontier-economics.com>
2024-07-31 16:40:22 +0100

    misc cleaning from JR

--- smf_data.R
+++ smf_data.R
@@ -54,20 +54,17 @@ time_start <- Sys.time()
 print.("start\n")
 
 
-## import data =================================================================
+## Import data =================================================================
 
 
-## Import new england data
-## want a list of school URNs and northing easting
-
+## 1.1 Import schools information ==============================================
 
 schools_info <- read_csv(file.path(config$data_dir, "england_school_information.csv")) %>%
   clean_names %>%
-  select(urn, schname, postcode)
+  select(urn, schname, postcode, admission = admpol)
 
 
-## import establishment set
-
+## 1.2. Import establishment set ===============================================
 
 schools_loc <- read_csv(file.path(config$data_dir, "establishment.csv")) %>%
   clean_names %>%
@@ -76,32 +73,19 @@ schools_loc <- read_csv(file.path(config$data_dir, "establishment.csv")) %>%
          easting)
 
 
+## 2. England deprivation ======================================================
+
+eng_deprivation <- read_csv(file.path(config$data_dir, "england_imd_idaci.csv"), show_col_types=FALSE) %>%
+  clean_names
 
 
-
-
-## 1. England deprivation ======================================================
-
-eng_deprivation <- file.path(config$data_dir, "English indices of deprivation 2019/File_1_-_IMD2019_Index_of_Multiple_Deprivation.xlsx") %>% 
-  readxl::read_excel(sheet = "IMD2019") %>% clean_names %>%
-  select(
-    lsoa_code_2011, 
-    lsoa_name_2011,
-    la_code_2019 = local_authority_district_code_2019,  
-    la_name_2019 = local_authority_district_name_2019,  
-    imd_rank = index_of_multiple_deprivation_imd_rank,
-    imd_decile = index_of_multiple_deprivation_imd_decile
-  ) 
-
-
-
-## 2. Scotland deprivation =====================================================
+## 3. Scotland deprivation =====================================================
 
 scot_deprevation <- file.path(config$data_dir, "data/School+level+summary+statistics+2023.xlsx") %>% 
   readxl::read_excel(sheet = "2023 School Level Statistics", skip =1)
 
 
-## 3. Welsh deprivation ========================================================
+## 4. Welsh deprivation ========================================================
 wales_deprivation <- file.path(config$data_dir, "data/Welsh_wmid.xlsx") %>% 
   readxl::read_excel(sheet = "Data", skip =3) %>% clean_names %>%
   select(
@@ -112,129 +96,60 @@ wales_deprivation <- file.path(config$data_dir, "data/Welsh_wmid.xlsx") %>%
   )
 
 
-## 4. IDACI of schools in England ==============================================
-idaci_deprivation <- file.path(config$data_dir, "File_3_-_IoD2019_Supplementary_Indices_-_IDACI_and_IDAOPI.xlsx") %>% 
-  readxl::read_excel(sheet = "IoD2019 IDACI & IDAOPI") %>% clean_names%>%
-  select(
-    lsoa_code_2011,
-    lsoa_name_2011,
-    la_code_2019 = local_authority_district_code_2019,  
-    la_name_2019 = local_authority_district_name_2019,  
-    imd_rank = index_of_multiple_deprivation_imd_rank_where_1_is_most_deprived,
-    imd_decile = index_of_multiple_deprivation_imd_decile_where_1_is_most_deprived_10_percent_of_lso_as,
-    icai_rank = income_deprivation_affecting_children_index_idaci_rank_where_1_is_most_deprived,
-    idaci_decile = income_deprivation_affecting_children_index_idaci_decile_where_1_is_most_deprived_10_percent_of_lso_as
-  ) 
-
-
-## merging the datasets 
-#merged_data <- left_join(idaci_deprivation, eng_deprivation,  by = c("lsoa_code_2011", "lsoa_name_2011", "la_code_2019", "la_name_2019"))
-
-# comparison_result <- merged_data %>%
-#  mutate(is_equal = if_else(imd_rank.x == imd_rank.y, TRUE, FALSE)) %>%
-#  summarise(all_equal = all(is_equal)) # all true, therefore only need idaci_deprivation :)
-
-
-
 
 ## 5. Number of students in school =============================================
-pupils_data <- read_csv(file.path(config$data_dir, "data/spc_school_characteristics_.csv")) %>%
+pupils_data <-  read_csv(file.path(config$data_dir, "updated/england_ks4final.csv"), show_col_types=FALSE) %>%
+  clean_names %>% 
    select(
-    time_period,  
-    region_code,  
-    region_name,
-    old_la_code,
-    la_code_2019 = new_la_code,  
-    la_name_2019 = la_name,
-    phase_type_grouping,
-    admissions_policy, 
-    headcount_of_pupils 
-  )
-
-## this is being subset by so many different things... 
-
-## 6. FSM pupils ===============================================================
-fsm_pupils_data <- read_csv(file.path(config$data_dir, "spc_pupils_fsm.csv")) %>%
-  select(
-    time_period,  
-    region_code,  
-    region_name,
-    old_la_code,
-    la_code_2019 = new_la_code,
-    la_name_2019 = la_name, 
-    phase_type_grouping,
-    fsm, 
-    headcount
-    ) %>%
-  filter(fsm == "Total")
-
-## do we care about phase_type_grouping? lists the different types of schools... 
+     urn, 
+     schname, 
+     postcode = pcode, 
+     constituency_code = pcon_code, 
+     constituency_name = pcon_name, 
+     total_pupils = totpups, 
+     fsm_share = ptfsm6cla1a, 
+     attain_8_score = att8scr_fsm6cla1a)
 
 
-## 7. Stage 4 progress =========================================================
-stage_4 <- read_csv(file.path(config$data_dir, "ks4_dm_ud_202122_la_rev.csv")) %>%
-  select(
-    time_period, 
-    old_la_code, 
-    la_code_2019 = new_la_code, 
-    la_name_2019 = la_name, 
-    institution_group,
-    breakdown_topic, 
-    breakdown, 
-    data_type, 
-    all_work, 
-    further_educ = fe, 
-    overall, 
-    appl2, 
-    appl3, 
-    appren
-         ) 
-
-## 8. Attainment scores ========================================================
-attainment <- read_csv(file.path(config$data_dir, "data-key-stage-4-performance.csv")) %>%
-  select(
-    time_period, 
-    la_code_2019 = lad_code, 
-    la_name_2019 = lad_name, 
-    free_school_meals, 
-    t_schools, 
-    t_pupils,
-    average_level8 = avg_att8
-  )
-
-
-    ## no indicator for phase type grouping :( 
-
-## 9. A level results ==========================================================
-a_level <- read_csv(file.path(config$data_dir, "data-a-level-and-other-16-to-18-results.csv")) %>%
-  select(
-    time_period, 
-    region_code, 
-    region_name, 
-    old_la_code, 
-    la_code_2019 = new_la_code, 
-    la_name_2019 = la_name, 
-    characteristic_type, 
-    number_of_students_alev, 
-    aps_per_entry_grade_alev
-  )
-
-## no indicator for phase type grouping :( 
-
-
-## 10. Parliamentary constituency ==============================================
-##TODO: add show_col_types to all read_csv (or make a wrapper function)
-constituency <- read_csv(file.path(config$data_dir, "parliamentary_constituency_csv.csv"), show_col_types=FALSE) %>% 
+## 5. A Level results ==========================================================
+a_level <- read_csv(file.path(config$data_dir, "updated/england_ks5final.csv"), show_col_types = FALSE) %>%
   clean_names %>%
-  select(
-    time_period, 
-    pcon_code, 
-    pcon_name, 
-    t_schools, 
-    avg_att8
-  )
+   select(
+     urn, 
+     schname, 
+     a_level_score = tallppegrd_alev_dis) 
 
 
+## 6. Appren/Uni ==== ==========================================================
+next_stage <- read_csv(file.path(config$data_dir, "updated/england_ks5-studest-he.csv"), show_col_types = FALSE) %>%
+  clean_names %>%
+   select(
+    urn, 
+    schname, 
+    progress_to_uni = dis_russell, 
+    progress_to_appren = dis_appren) 
+
+
+
+
+## merge step by step first to check all okay
+
+# merge eng deprivation to postcode
+schools_info <- schools_info %>% 
+  left_join(eng_deprivation,  by = c("postcode")) 
+## I get a warning here - check this
+
+
+schools_info <- schools_info %>% 
+  left_join(pupils_data, by = c("urn", "postcode", "schname"))
+
+
+schools_info <- schools_info %>% 
+  left_join(a_level, by = c("urn", "schname"))
+
+
+schools_info <- schools_info %>% 
+  left_join(next_stage, by = c("urn", "schname"))
 
 
 ## create output tables ========================================================
