38c22de0925607e7db80625db0afe4ed01674635
Alex Latti <alex.latti@frontier-economics.com>
2024-08-22 10:20:46 +0100

    JR changes - add Wales and Scotland, Address Esther comments

--- smf_data.R
+++ smf_data.R
@@ -9,6 +9,7 @@ library(janitor)
 library(sf)
 library(htmltools)
 library(purrr)
+library(stringr)
 
 options(stringsAsFactors=FALSE)
 options(scipen=999)
@@ -21,7 +22,7 @@ time_start <- Sys.time()
 
 ## project parameters ==========================================================
 config <- list(
-  version="v02",
+  version="v04",
   data_dir=here("data"),
   draw_charts=TRUE,
   refresh_cache=FALSE
@@ -83,48 +84,113 @@ i <- list()
 
 ## 1.1 Import schools information ==============================================
 i$schools_info <- read_csv.("england_school_information.csv") %>%
-  filter(ispost16 == 1) %>%     ## want to keep only those schools which have 16+ year olds
-  select(urn, schname, postcode, admission=admpol)
+  filter(minorgroup != "Independent school") %>% # remove independent schools
+  filter(issecondary == 1) %>%     ## want to keep only those schools which have 16+ year olds
+  select(urn, schname, postcode, admission=admpol,
+         ispost16)
 
 
 ## 1.2. Import establishment set ===============================================
 i$schools_loc <- read_csv.("establishment.csv") %>%
+  filter(establishment_type_group_name != "Independent schools") %>%
   select(urn, northing, easting)
 
 
-## 1.3. Import Schools Contact info ============================================
+## 1.3. Import English Schools Contact info =====================================
 i$schools_contact <- read_csv.("English schools list.csv", encoding="ISO-8859-1") %>%   
   mutate(head_teacher = paste(head_first_name, head_last_name)) %>% 
   select(urn, main_email, telephone_num, head_teacher)
 
-## 2. England deprivation ======================================================
+## can filter for indep schools here too if needed. 
+
+## 2. Import England deprivation ===============================================
 
 i$eng_deprivation <- read_csv.("england_imd_idaci.csv") %>% 
-  distinct() 
-
-# have checked that the duplicates are duplicated rows, and not duplicated postcodes. 
+  distinct() # duplicates are duplicated rows, not duplicated postcodes. 
 
 
-## 3. Scotland deprivation =====================================================
+## 3. Import Scotland data =====================================================
 
-## AL: commented out until cleaned - otherwise throws errors in the input check
- #i$scot_deprevation <- file.path(config$data_dir, "data/School+level+summary+statistics+2023.xlsx") %>% 
-  # readxl::read_excel(sheet = "2023 School Level Statistics", skip =1)
-#this file does not have post codes, only at the local authority level, searching for data 
+## 3.1. Import Scotland deprivation ============================================
+ i$scot_deprivation <- file.path(config$data_dir, "data/School+level+summary+statistics+2023.xlsx") %>% 
+   readxl::read_excel(sheet = "2023 School Level Statistics", skip =1) %>%
+   filter(`School Type` == "Secondary") %>% clean_names() %>% 
+   select(urn = seed_code,
+          la_name = local_authority,
+          schname = school_name, 
+          total_pupils = pupil_roll, 
+          fsm_share = percentage_of_p6_p7_s1_s6_sp_pupils_registered_for_free_school_meals_note_5,
+          simd_quintile_1 = simd_quintile_1_note_6,
+          simd_quintile_2 = simd_quintile_2_note_6,
+          simd_quintile_3 = simd_quintile_3_note_6,
+          simd_quintile_4 = simd_quintile_4_note_6,
+          simd_quintile_5 = simd_quintile_5_note_6)
 
-## 4. Welsh deprivation ========================================================
-##i$wales_deprivation <- file.path(config$data_dir, "data/Welsh_wmid.xlsx") %>% 
-#  readxl::read_excel(sheet = "Data", skip =3) %>% clean_names %>%
-#  select(
-#    lsoa_code, ## no la codes here
-#    la_name = local_authority_name, 
-#    lsoa_name,
-#    wimd_2019
-#  )
 
-# only at the lsoa code level, will search for data
+## 3.2. Import Scotland contacts and long/lat ==================================
+i$scot_contact <- file.path(config$data_dir, "scot_SchoolRoll_2023.xlsx") %>% 
+  readxl::read_excel(sheet = "Schools_Final") %>%
+  filter(SchoolType == "Secondary") %>% clean_names() %>% 
+  select(urn = seed_code,
+         postcode = post_code, 
+         pc_latitude = latitude, 
+         pc_longitude = longitude,
+         main_email = email, 
+         telephone_num = phone)
 
-## 5. Number of students in school =============================================
+
+
+## 4. Import Welsh data ========================================================
+
+## 4.1. Import Welsh deprivation ===============================================
+i$wales_deprivation <- file.path(config$data_dir, "data/Welsh_wmid.xlsx") %>% 
+  readxl::read_excel(sheet = "Data", skip =3) %>% clean_names %>%
+  select(lsoa_code, 
+         la_name = local_authority_name, 
+         wimd_2019) 
+
+## 4.2. Import Welsh fsm share =================================================
+wales_fsm <- read_csv.("Welsh data/fsm_1.csv") %>% 
+  clean_names %>% 
+  select(la_name = name,
+         fsm_share = x_of_secondary_school_pupils_who_are_eligible_for_free_school_meals_ay_21_22)
+
+
+## 4.3. Import Schools location data ===========================================
+wales_maint_loc <- file.path(config$data_dir, "Welsh data/address-list-schools-wales_0.xlsx") %>% 
+  readxl::read_excel(sheet = "Maintained") %>% clean_names %>%
+  filter(school_type == "Secondary (ages 11-19)") %>% 
+  select(urn = school_number, 
+         schname = school_name, 
+         la_name = local_authority, 
+         postcode, 
+         telephone_num = phone_number,
+         total_pupils = pupils_see_notes) %>% 
+  mutate(telephone_num = str_replace_all(telephone_num, " ", "")
+  )
+
+
+
+## join Wales data to our list 
+i$wales_deprivation <- wales_maint_loc %>% 
+  left_join(i$wales_deprivation, by = "la_name")
+## expect multiple matches, since wales_maint_loc does not have unique la_name
+
+
+## 4.4. Aggregate wimd to la level, to add to map ==============================
+wimd_weighted_average <- i$wales_deprivation %>%
+  group_by(la_name) %>% 
+  summarise(weighted_wimd = sum(wimd_2019 * total_pupils) /sum(total_pupils)) 
+## using total_pupils as a proxy for la population
+
+## merge back onto our list
+i$wales_deprivation <- i$wales_deprivation %>%
+  left_join(wimd_weighted_average, by = "la_name") %>%
+  select(-lsoa_code, -wimd_2019) %>%
+  distinct()
+
+
+## 5. Number of students in English schools ====================================
 i$pupils_data <-  read_csv.("updated/england_ks4final.csv") %>%
   select(
      urn, 
@@ -136,7 +202,7 @@ i$pupils_data <-  read_csv.("updated/england_ks4final.csv") %>%
   filter(!is.na(urn)) ##NA URNs are associated with aggregated local authority data
 
 
-## 5. A Level results ==========================================================
+## 6. A Level results ==========================================================
 i$a_level <- read_csv.("updated/england_ks5final.csv") %>%
    select(
      urn, 
@@ -145,16 +211,30 @@ i$a_level <- read_csv.("updated/england_ks5final.csv") %>%
 
 
 
-## 6. Appren/Uni ==== ==========================================================
+## 7. Appren/Uni ===============================================================
 i$next_stage <- read_csv.("updated/england_ks5-studest-he.csv") %>%
    select(
     urn, 
-    progress_to_uni = dis_russell, 
-    progress_to_appren = dis_appren) %>% 
+    progress_to_uni = all_progressed, 
+    progress_to_appren = all_appren,
+    institution_type = nftype) %>% 
+  mutate(institution_type = recode(institution_type,
+                                   "AC" = "Academy Sponsor Led",
+                                   "ACC" = "Academy Sponsor Led",
+                                   "AC1619" = "Academy Sponsor Led", 
+                                   "ACC1619" = "Academy 16-19 Converter",
+                                   "CY" = "Community School", 
+                                   "SS" = "Studio School", 
+                                   "VC" = "Voluntary Controlled School", 
+                                   "VA" = "Voluntary Aided School", 
+                                   "F1619" = "Free School - 16-19", 
+                                   "FD" = "Foundation School",
+                                   "F" = "Free School", 
+                                   "CTC" = "City Technology College")) %>%
   filter(!is.na(urn)) ##NA URNs are associated with aggregated local authority data
 
 
-## 7. Post code lat / lon data =================================================
+## 8. Post code lat / lon data =================================================
 i$post_code_lat_long <-  read_csv.("postcode_long_lat.csv") %>%
   select(
     postcode, 
@@ -163,11 +243,18 @@ i$post_code_lat_long <-  read_csv.("postcode_long_lat.csv") %>%
 
 
 ## Import constituency shape file ==============================================
-## using the latest constituencies
 constituency_shape <- st_read(file.path(config$data_dir, "PCON_DEC_2020_UK_BFC.shp"), quiet=T) %>% 
   st_transform(crs=4326) %>% 
   select(constituency_code=PCON20CD, constituency_name=PCON20NM)
 
+
+## Import local authority shape file ===========================================
+local_authority_shape <- st_read(file.path(config$data_dir, "LAD_MAY_2024_UK_BFC.shp"), quiet = T) %>%
+  st_transform(crs=4326)
+
+# finding the centre point of local authorities 
+local_authority_centre <- local_authority_shape %>%
+  st_centroid()
 
 ## perform standard input checks ===============================================
 for (df_name in names(i)){
@@ -193,28 +280,81 @@ d <- list()
 
 ## test code to account for missing northing and easting
 schools <- i$schools_info %>%
+  left_join(i$eng_deprivation, by = c("postcode")) %>%
   left_join(i$schools_contact, by = c("urn")) %>%
   left_join(i$pupils_data, by = c("urn")) %>%
   left_join(i$a_level, by = c("urn")) %>%
   left_join(i$next_stage, by = c("urn")) %>%
-  # left_join(i$eng_deprivation, by = c("postcode")) %>%
   left_join(i$schools_loc, by = c("urn")) %>%
+  full_join(i$scot_deprivation, by =c("urn")) %>%
+  left_join(i$scot_contact, by = c("urn")) %>%
+  full_join(i$wales_deprivation, by = c("urn")) %>% 
   left_join(i$post_code_lat_long, by = c("postcode")) 
 
+
+
+
+
+
+  
+    ## using schools_check to add our data together 
+schools_check <- schools %>%
+  mutate(
+    fsm_share.x = as.numeric(gsub("%", "", fsm_share.x)), 
+    telephone_num.x = as.character(telephone_num.x),
+    telephone_num.y = as.character(telephone_num.y),
+    main_email.x = as.character(main_email.x),
+    main_email.y = as.character(main_email.y),
+    total_pupils.x = as.numeric(total_pupils.x),
+    total_pupils.y = as.numeric(total_pupils.y),
+    fsm_share.x = as.numeric(fsm_share.x),
+    fsm_share.y = as.numeric(fsm_share.y),
+    la_name.x = as.character(la_name.x),
+    la_name.y = as.character(la_name.y),
+    pc_latitude.x = as.numeric(pc_latitude.x),
+    pc_latitude.y = as.numeric(pc_latitude.y),
+    pc_longitude.x = as.numeric(pc_longitude.x),
+    pc_longitude.y = as.numeric(pc_longitude.y)
+  ) %>%
+  mutate(
+    schname = coalesce(schname.x, schname.y), 
+    postcode = coalesce(postcode.x, postcode.y), 
+    main_email = coalesce(main_email.x, main_email.y), 
+    telephone_num = coalesce(telephone_num.x, telephone_num.y), 
+    total_pupils = coalesce(total_pupils.x, total_pupils.y), 
+    fsm_share = coalesce(fsm_share.x, fsm_share.y), 
+    la_name = coalesce(la_name.x, la_name.y), 
+    pc_latitude = coalesce(pc_latitude.x, pc_latitude.y), 
+    pc_longitude = coalesce(pc_longitude.x, pc_longitude.y)
+  ) %>%
+  # remove original columns
+  select(-schname.x, -schname.y, 
+         -postcode.x, -postcode.y,
+         -main_email.x, -main_email.y, 
+         -telephone_num.x, -telephone_num.y, 
+         -total_pupils.x, -total_pupils.y, 
+         -fsm_share.x, -fsm_share.y, 
+         -la_name.x, -la_name.y, 
+         -pc_latitude.x, -pc_latitude.y, 
+         -pc_longitude.x, -pc_longitude.y)
+
+
+
 ## add some geometry
-with_easting_northing <- schools %>%
+with_easting_northing <- schools_check %>%
   filter(!is.na(easting) & !is.na(northing)) %>%
   st_as_sf(coords = c("easting", "northing"), crs = 27700) %>%
   st_transform(crs = 4326)
 
 ## if no location of school is available, use the postcode location
-with_postcode_long_lat <- schools %>%
+with_postcode_long_lat <- schools_check %>%
   filter((is.na(easting) | is.na(northing)) & !is.na(pc_longitude) & !is.na(pc_latitude)) %>%
   st_as_sf(coords = c("pc_longitude", "pc_latitude"), crs = 4326)
 
 d$display_names <- c(
   "urn"="URN",
   "schname"="School name",
+  "institution_type" = "School type",
   "head_teacher"="Head Teacher",
   "main_email"="Email Address",
   "telephone_num"="Contact Number",
@@ -227,10 +367,15 @@ d$display_names <- c(
   "attain_8_score"="Attain 8 score",
   "a_level_score"="A level score",
   "progress_to_uni"="Progress to Uni",
-  "progress_to_appren"="Progress to apprenticeship"
-  # "idaci_decile" = "IDACI decile", 
-  # "index_of_multiple_deprivation_decile" = "IMD decile"
-)
+  "progress_to_appren"="Progress to apprenticeship",
+  "idaci_decile" = "IDACI decile", 
+  "index_of_multiple_deprivation_decile" = "IMD decile", 
+  "simd_quintile_1" = "Scottish IMD quintile 1", 
+  "simd_quintile_2" = "Scottish IMD quintile 2", 
+  "simd_quintile_3" = "Scottish IMD quintile 3", 
+  "simd_quintile_4" = "Scottish IMD quintile 4", 
+  "simd_quintile_5" = "Scottish IMD quintile 5", 
+  "weighted_wimd" = "Welsh IMD")
 
 d$na_codes <- c(
   "NP"="Not Provided",
@@ -241,7 +386,7 @@ d$na_codes <- c(
 )
 
 
-d$labels <- schools %>% 
+d$labels <- schools_check %>% 
   st_drop_geometry() %>% 
   ## attain_8_score appears to be a percentage, so adding it on here
   mutate(attain_8_score = if_else(is.na(attain_8_score) | attain_8_score=="" | attain_8_score %in% names(d$na_codes), 
@@ -282,8 +427,9 @@ a_level_score_levels <- c(
   "E-",
   "x"
 )
+
 clean_na <- function(col) if_else(col %in% names(d$na_codes) | col=="", NA, col)
-rem_perc <- function(col) gsub("%", "", col)
+rem_perc <- function(col) gsub("%", "", col) 
 ## combine geo locations and convert to numeric.
 d$schools_point <- bind_rows(with_easting_northing, with_postcode_long_lat) %>% 
   select(-easting, -northing, -pc_longitude, -pc_latitude) %>% 
@@ -293,7 +439,10 @@ d$schools_point <- bind_rows(with_easting_northing, with_postcode_long_lat) %>%
          attain_8_score = as.numeric(clean_na(attain_8_score)) / 100, ## appears to be % even if not showing a % sign
          a_level_score = factor(clean_na(a_level_score), levels=rev(a_level_score_levels)), ## order from least to most to match the slider order in the map
          progress_to_uni = as.numeric(rem_perc(clean_na(progress_to_uni))) / 100,
-         progress_to_appren = as.numeric(rem_perc(clean_na(progress_to_appren))) / 100) %>% 
+         progress_to_appren = as.numeric(rem_perc(clean_na(progress_to_appren))) / 100, 
+         ## add new data points here
+         index_of_multiple_deprivation_decile = as.numeric(clean_na(index_of_multiple_deprivation_decile))
+         ) %>% 
   st_join(constituency_shape %>% select(within_code=constituency_code, within_name=constituency_name), left=F) %>% 
   left_join(constituency_shape %>% st_drop_geometry() %>%  select(constituency_code, match_name=constituency_name), by="constituency_code") %>% 
   ## for all matched shapes, accept the shape name
